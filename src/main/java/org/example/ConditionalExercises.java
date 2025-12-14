931c72f29a899bc6a0a0dbfe0dfa6d0ea4252e91
Jaya <jaya.kolte21@gmail.com>
2025-12-14 14:19:49 -0600

    Required exercises completed

--- src/main/java/org/example/ConditionalExercises.java
+++ src/main/java/org/example/ConditionalExercises.java
@@ -1,9 +1,16 @@
 package org.example;
 
+
 public class ConditionalExercises {
+
+    //Scanner sc = new Scanner(System.in);
     public boolean lessThanFive(int x) {
         // Replace the line below with code that returns true if x is less than 5, and false otherwise
         // (use an if statement with an else block.)
+
+        if(x <5){
+            return true;
+        }else
         return false;
     }
 
@@ -11,8 +18,15 @@ public class ConditionalExercises {
         // Replace the line below with code that returns "child" if age is less than 13,
         // "teen" if age is less than 20, and "adult" otherwise
         // (use if, else if, and else)
-        return "";
-    }
+        if(age < 13)
+            return "child";
+        else if (age>13 && age<20) {
+            return "teen";
+             }
+        else
+            return "adult";
+
+            }
 
     public boolean isValidPassword(String password) {
         int passwordLength = password.length();
@@ -20,6 +34,9 @@ public class ConditionalExercises {
         // Replace the line below with code that returns true if password is at least 8 characters long
         // and false otherwise
         // (use an if statement with an else block.)
+        if(passwordLength >=8)
+            return true;
+        else
         return false;
     }
 }
